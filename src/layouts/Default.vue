0f5e9d75c1df30a6cc9ddffa3a3cbde88b5f423e
Cristopher Paniagua <cristopher.p.s.cto@gmail.com>
2021-08-20 19:31:17 -0500

    Creates default layout for pages

---
+++ src/layouts/Default.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <header class="bg-light py-3">
+      <h1 class="text-white text-center text-primary">
+        <i class="fas fa-address-book text-primary"></i>
+        CONTACTS
+      </h1>
+    </header>
+    <div class="container">
+      <navbar-vue/>
+      <hr>
+      <slot/>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Default',
+};
+</script>
