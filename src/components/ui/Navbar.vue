304955e5bd7b7d527e3494afd327e2fcfb65b7ee
Cristopher Paniagua <cristopher.p.s.cto@gmail.com>
2021-08-20 19:26:38 -0500

    Create Navbar.vue

---
+++ src/components/ui/Navbar.vue
@@ -0,0 +1,43 @@
+<template>
+  <ul class="nav mt-2">
+    <li class="nav-item">
+      <router-link
+        to="/"
+        v-slot="{ route, isActive }"
+        custom
+      >
+        <a
+          :class="isActive ? 'nav-link active' : 'nav-link'"
+          :href="route.fullPath"
+        >
+          <i class="fas fa-home"/>
+          Home
+        </a>
+      </router-link>
+    </li>
+    <li class="nav-item">
+      <router-link
+        to="/form"
+        v-slot="{ route, isActive }"
+        custom
+      >
+        <a
+          :class="isActive ? 'nav-link active' : 'nav-link'"
+          :href="route.fullPath"
+        >
+          <i class="fas fa-address-book"></i>
+          New contact
+        </a>
+      </router-link>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  name: 'Navbar',
+};
+</script>
+
+<style>
+</style>
