4c4b8c79f79531512cfc03021e38f891328cb0f1
Cristopher Paniagua <cristopher.p.s.cto@gmail.com>
2021-08-20 19:32:14 -0500

    Create Error.vue

---
+++ src/pages/Error.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="container">
+    <div class="row text-center mt-5">
+      <h1 class="text-danger">
+        <i class="fas fa-bug"></i>
+        Página no encontrada
+      </h1>
+      <a href="/">
+        <i class="fas fa-home"></i>
+        Ir a inicio
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Error',
+};
+</script>
